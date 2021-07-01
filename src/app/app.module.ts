ca39ff5f6b910a8d58ba3e65df92cfdbea178fe2
Akram Gorashi <akramgorashi6@hotmail.com>
2021-07-02 00:34:05 +0200

    [update] added header , sidebar , register new rider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegisterRiderComponent } from './components/register-rider/register-rider.component';
 import { RidersListComponent } from './components/riders-list/riders-list.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RidersListComponent
+    RidersListComponent,
+    HeaderComponent,
+    RegisterRiderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/register-rider/register-rider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-rider',
+  templateUrl: './register-rider.component.html',
+  styleUrls: ['./register-rider.component.scss']
+})
+export class RegisterRiderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
